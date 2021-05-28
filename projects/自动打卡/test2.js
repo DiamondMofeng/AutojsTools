1b4b4c33d7bffa2cd8ae3a5ae483a24015e528a6
Mofeng <363034937@qq.com>
2021-05-28 11:58:25 +0800

    nothing

--- projects/自动打卡/test2.js
+++
@@ -1,6 +0,0 @@
-var yiqingxixishouji = text("疫情信息收集").findOnce();
-if (yiqingxixishouji){
-    click(yiqingxixishouji.bounds().centerX(), yiqingxixishouji.bounds().centerY()  );
-
-    
-}
--- workbench/test/tst.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function(){
-    //这里的语句会在15秒后执行而不是5秒后
-    toast("hello")
-}, 5000);
-//暂停10秒
