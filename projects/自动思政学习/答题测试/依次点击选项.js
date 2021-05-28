1c549064cefe9707a133aff56f57b5ce8483b8e9
Mofeng <363034937@qq.com>
2021-05-29 04:19:31 +0800

    workinprogress 3.2.1

---
+++ projects/自动思政学习/答题测试/依次点击选项.js
@@ -0,0 +1,9 @@
+i=0
+
+for (i=0;i<4;i++){
+
+    var opt = className("android.widget.RadioButton").findOnce(i);
+    click(opt.bounds().centerX(), opt.bounds().centerY());
+    sleep(2000);
+    }
+
